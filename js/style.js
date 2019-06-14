20de4920930fbaf96ec127382dc4e82d2d6c3a17
human97 <xinfoz19@gmail.com>
2019-06-14 17:00:43 +0400

    js beautify

--- js/style.js
+++ js/style.js
@@ -1,13 +1,13 @@
-window.onload = function(){
-    let menuStyle=getComputedStyle(menu);
-    openMenu=document.getElementById('openMenu');
-    openMenu.onclick=function(){
-        if(menuStyle.display=='none'){
-          menu.classList.add('active');
-          this.textContent='Close Menu';  
-        } else{
-            menu.classList.remove('active');
-            this.textContent='Open Menu';  
-          }
+window.onload = function () {
+  let menuStyle = getComputedStyle(menu);
+  openMenu = document.getElementById('openMenu');
+  openMenu.onclick = function () {
+    if (menuStyle.display == 'none') {
+      menu.classList.add('active');
+      this.textContent = 'Close Menu';
+    } else {
+      menu.classList.remove('active');
+      this.textContent = 'Open Menu';
     }
+  }
 }
